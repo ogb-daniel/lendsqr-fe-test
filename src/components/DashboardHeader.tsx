02dfe9a1cdbcf76d856e329f2124aa7a48f9b5b5
ogb-daniel <ogbuikedaniel@gmail.com>
2023-01-29 12:18:44 +0100

    changes to mobile design

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -10,17 +10,16 @@ interface DashboardHeaderProps {
 
 export const DashboardHeader: React.FC<DashboardHeaderProps> = ({}) => {
     const [search, setSearch] = useState(""); 
-    
     return (
             <div className='dashboard__header'>
-                 <div className="menu-btn">
+                 {/* <div className="menu-btn">
             <span className="menu-btn__burger"></span>
-        </div>
+        </div> */}
                 <img src={logo} alt="logo" />
                 <Input value={search} onChange={(e:ChangeEvent<HTMLInputElement>)=>{
                     setSearch(e.target.value)
-                }} className="nav" placeholder='Search for anything' type='text' search={true} />
-                <div className='nav dashboard__header__right'>
+                }} className="" placeholder='Search for anything' type='text' search={true} />
+                <div className=' dashboard__header__right'>
                     <li className='docs'><a href="#">Docs</a></li>
                     <img className='bell' src={bell} alt="notifications" />
                     <div className='profile'>
